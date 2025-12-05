8352b662392576e211d724d572bc122291a2a457
Paul Virally <pvirally@gmail.com>
2025-12-05 10:40:49 -0500

    Initial commit

---
+++ src/MatrixFreeRandomizedLinearAlgebra.jl
@@ -0,0 +1,31 @@
+"""
+    module MatrixFreeRandomizedLinearAlgebra
+
+Tools for matrix-free randomized linear algebra supporting both CPU and GPU arrays.
+
+This module provides:
+
+    * [`rsvd`](@ref) / [`rsvdvals`](@ref): randomized SVD and singular values
+    for general (possibly rectangular) operators, using power iteration and
+    oversampling.
+    * [`reigen_hermitian`](@ref) / [`reigenvals_hermitian`](@ref): randomized
+    eigen-decomposition and eigenvalues for Hermitian operators.
+
+All routines are written to work with standard `AbstractMatrix` types (e.g., Matrix, CuMatrix), but also the types from [LinearMaps.jl](https://julialinearalgebra.github.io/LinearMaps.jl/stable/), or more generally any type supporting
+
+  * size query `size(operator)` and
+  * multiply `operator * X`
+  * adjoint multiply `operator' * X`
+
+You can optionally pass a `sample_vec` prototype to place all temporary work
+arrays on a specific device (e.g. GPU vs CPU).
+"""
+module MatrixFreeRandomizedLinearAlgebra
+
+include("rsvd.jl")
+export rsvd, rsvdvals
+
+include("reigen.jl")
+export reigen_hermitian, reigvals_hermitian
+
+end # module MatrixFreeRandomizedLinearAlgebra
