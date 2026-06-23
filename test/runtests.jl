19c7a4a6f7d1f43f862441982b1ed5ab74c94a58
Paul Virally <pvirally@gmail.com>
2026-06-23 14:09:19 -0400

    Add Aqua.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,7 @@ using Test
 using MatrixFreeRandomizedLinearAlgebra
 
 @testset "MatrixFreeRandomizedLinearAlgebra" begin
+    include("aqua_tests.jl")
     include("rsvd_tests.jl")
     include("rsvdvals_tests.jl")
     include("reigen_hermitian_tests.jl")
